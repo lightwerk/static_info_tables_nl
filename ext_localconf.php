447dbc34bb8d0d2886ec0c9bca66f88798f4b465
mafl <mafl@users.noreply.github.com>
2021-04-12 10:48:15 +0200

    Update ext_localconf.php
    Include TYPOSCRIPT for v10.x

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,9 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/Extbase/setup.txt">');
+call_user_func(function($extensionKey) {
+    // Deprecated since 10.0 @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.txt">'
+    );
+}, 'static_info_tables_nl');
